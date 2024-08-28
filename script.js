891a31b4a7e3a1916a84c26f28811309059ae8af
Menimobong Utibe-Ukim <menimobong@berkeley.edu>
2024-08-28 01:03:45 -0700

    Added change color, clear grid, and eraser buttons along with a slider that changes the grid size

--- script.js
+++ script.js
@@ -3,22 +3,81 @@ const gridContainer = document.querySelector(".grid-container");
 const computedStyles = window.getComputedStyle(gridContainer);
 const containerWidth = parseFloat(computedStyles.getPropertyValue("width"));
 
-const gridSize = 16;
+let gridSize = 16;
 
-const gridItemWidth = containerWidth / gridSize;
+const gridSlider = document.querySelector("#gridRange");
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item");
-    gridItem.style.width = `${gridItemWidth}px`;
-    gridItem.style.height = `${gridItemWidth}px`;
-    gridContainer.appendChild(gridItem);
+gridSlider.value = "16";
+const sliderValue = document.querySelector("#sliderValue");
+
+let color = "#F33A6A";
+document.querySelector(".colorShow").style.backgroundColor = color;
+document.querySelector("#colorPicker").value = color;
+
+constructGrid();
+
+function constructGrid() {
+    gridContainer.style.setProperty('--grid-size', gridSize);
+    let gridItemWidth = containerWidth / gridSize;
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item");
+        gridItem.style.width = `${gridItemWidth}px`;
+        gridItem.style.height = `${gridItemWidth}px`;
+        gridContainer.appendChild(gridItem);
+    }
+    buttons();
 }
 
-const gridItems = document.querySelectorAll(".grid-item");
+gridSlider.oninput = function () {
+    gridSize = this.value;
+    sliderValue.textContent = `${this.value} x ${this.value}`;
+    gridContainer.innerHTML = "";
+    constructGrid();
+};
 
-gridItems.forEach((gridItem) => {
-    gridItem.addEventListener("mouseenter", () => {
-        gridItem.style.backgroundColor = "#B68990"
+function buttons() {
+    const gridItems = document.querySelectorAll(".grid-item");
+
+    let isEraser = false;
+
+    const clearGrid = document.querySelector(".clearBtn");
+    clearGrid.addEventListener("click", () => {
+        gridItems.forEach((gridItem) => {
+            gridItem.style.backgroundColor = "white";
+        });
     });
-});
+
+    const eraserBtn = document.querySelector(".eraserBtn");
+    const eraserStyle = window.getComputedStyle(eraserBtn);
+    eraserBtn.addEventListener("click", () => {
+        isEraser = !isEraser;
+        const temp = eraserStyle.color;
+        eraserBtn.style.color = eraserStyle.backgroundColor;
+        eraserBtn.style.backgroundColor = temp;
+    });
+
+    const colorBtn = document.querySelector(".colorBtn");
+    colorBtn.addEventListener("click", () => {
+        document.querySelector("#colorPicker").click();
+    });
+
+    document
+        .querySelector("#colorPicker")
+        .addEventListener("input", function () {
+            let selectedColor = this.value;
+            color = selectedColor;
+            document.querySelector(".colorShow").style.backgroundColor = color;
+        });
+
+    gridItems.forEach((gridItem) => {
+        gridItem.addEventListener("mouseenter", () => {
+            if (!isEraser) {
+                gridItem.style.backgroundColor = color;
+            } else {
+                gridItem.style.backgroundColor = "white";
+            }
+        });
+    });
+}
